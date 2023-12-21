3dcd815dbf4cb7262a64e50cf684c37588acd032
Fabian Jocks <24557998+iamfj@users.noreply.github.com>
2023-12-21 13:43:59 +0000

    chore: adjusted release config

--- release.config.js
+++ release.config.js
@@ -6,6 +6,22 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    ['@semantic-release/npm', {npmPublish: false}],
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          'package.json',
+          'package-lock.json',
+          'dist/index.js',
+          'dist/licenses.txt',
+          'CHANGELOG.md',
+        ],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
     [
       '@semantic-release/github',
       {
